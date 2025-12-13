eb219f15d6a7a03508c5faa1f7fbba919eb5ab5b
NicoBoscia <135171592+NicoBoscia@users.noreply.github.com>
2025-12-12 23:39:54 -0300

    cambios program

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -126,14 +126,11 @@ app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "HairSync API V1");
-    c.RoutePrefix = string.Empty;
 });
 
 app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
-
-//app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin);
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
